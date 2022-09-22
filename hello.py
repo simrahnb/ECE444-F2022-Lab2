4d23ce0ba46423e13efc2685b914972ac4a4eaed
sim <simrahnajeeb@gmail.com>
2022-09-22 17:25:28 -0400

    Activity 4- Part 1 done - Reproduced example 4-7

--- hello.py
+++ hello.py
@@ -2,20 +2,37 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment 
 from datetime import datetime 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+ 
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name, current_time=datetime.utcnow())
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'),404
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ' ' 
+	return render_template('index.html', form=form, name=name)
+
+
+#@app.route('/')
+#def index():
+#    return render_template('index.html', current_time=datetime.utcnow())
+#@app.route('/user/<name>')
+#def user(name):
+ ##   return render_template('user.html', name=name, current_time=datetime.utcnow())
+#@app.errorhandler(404)
+#def page_not_found(e):
+ #   return render_template('404.html'),404
 
