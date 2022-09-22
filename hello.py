f0b7f6c31c81e8e7843970de385d4fbe37dc0e78
sim <simrahnajeeb@gmail.com>
2022-09-22 17:56:35 -0400

    Made a correction- successfully reproduced Example 4-7

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask import Flask, render_template, session, redirect, url_for, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -18,13 +19,14 @@ class NameForm(FlaskForm):
  
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ' ' 
-	return render_template('index.html', form=form, name=name)
-
+		old_name = session.get('name') 
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data 
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name = session.get('name'))
 
 #@app.route('/')
 #def index():
